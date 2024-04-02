94e5f3dff6e80de2e68c7adc781c2dfe17dcc742
Davasz <davidavi200615@gmail.com>
2024-04-02 11:53:51 -0300

    ✨ Feat: Search component

---
+++ client/src/components/TheSearch.vue
@@ -0,0 +1,29 @@
+<template>
+    <input type="text" placeholder="Enter the game">
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style scoped>
+
+input {
+    font-family: 'Trueno';
+    width: 85vw;
+    height: 2.2rem;
+    background-color: #01080D;
+    color: #758DA9;
+    font-size: 1em;
+    border: 1px #ffffff solid;
+    outline: none;
+    border-radius: 46px;
+    padding: 1.2rem;
+    box-sizing: border-box;
+    margin-top: 2rem;
+}
+
+
+</style>
