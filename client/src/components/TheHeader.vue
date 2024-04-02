924bbc01944f9c0b64c31b388d16c6044f78eef2
Davasz <davidavi200615@gmail.com>
2024-04-02 11:53:43 -0300

    ✨ Feat: Header component

---
+++ client/src/components/TheHeader.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+        <img src="../assets/img/icons/user-icon.svg" alt="">
+    </header>
+</template>
+
+<style scoped>
+
+    header {
+        display: flex;
+        width: 90vw;
+        margin: auto;
+        margin-top: 2rem;
+        justify-content: end;
+    }
+
+</style>
