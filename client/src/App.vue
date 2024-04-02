f3c4b2c7298518e8b8dc7e6d566cbc4fc6a91a36
Davasz <davidavi200615@gmail.com>
2024-04-02 11:54:29 -0300

    ✨ Feat: Config app template

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,30 +1,21 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+  <TheHeader/>
+  
+  <HomeView/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
 
-nav {
-  padding: 30px;
-}
+// Import global css
+import '@/assets/css/global.css'
+import TheHeader from '@/components/TheHeader.vue'
+import HomeView from '@/views/HomeView.vue'
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
+export default {
+  name: 'App',
+  components: {
+    TheHeader,
+    HomeView
+  },
 }
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
