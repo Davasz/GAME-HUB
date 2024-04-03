d8738f2ba23a581bf21dc0c45824869f087860bd
Davasz <davidavi200615@gmail.com>
2024-04-03 11:29:12 -0300

    ⚙️Feat: Loading Animation, search methods, event listenings

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -2,19 +2,23 @@
   <main>
     <h1>Browse games</h1>
     <div class="categories">
-      <div class="category">Action</div>
-      <div class="category">Indie</div>
-      <div class="category">RPG</div>
-      <div class="category">Shooter</div>
-      <div class="category">Casual</div>
-      <div class="category">Puzzle</div>
+      <div @click="onGenres('all')" class="category">All</div>
+      <div @click="onGenres('action')" class="category">Action</div>
+      <div @click="onGenres('indie')" class="category">Indie</div>
+      <div @click="onGenres('arcade')" class="category">arcade</div>
+      <div @click="onGenres('shooter')" class="category">Shooter</div>
+      <div @click="onGenres('casual')" class="category">Casual</div>
+      <div @click="onGenres('puzzle')" class="category">Puzzle</div>
     </div>
 
-    <TheSearch />
+    <TheSearch @searched="searched($event)" @onSearch="onSearch($event)" />
 
-    <h1 class="tittle-medium">Top games</h1>
+    <img v-if="showAnimation" class="loading-animation" src="../assets/img/animation/loading-animation.svg"
+      alt="loading animation">
 
-    <section>
+    <section v-if="!isOnSearch">
+      <h1 class="tittle-medium">Top games</h1>
+
       <div class="container">
         <div class="cards">
           <TopGamesCard v-for="(game, index) in store.state.topGames" :key="index" :imageUrl="game.background_image">
@@ -32,10 +36,10 @@
       </div>
     </section>
 
-    <section>
-      <h1 class="tittle-medium">Recomended Games</h1>
+    <section v-if="isOnSearchGender">
+      <h1 class="tittle-medium">{{ gameTypeTittle }} Games</h1>
 
-      <GameCard v-for="(game, index) in store.state.recomendedGames" :key="index" :imageUrl="game.background_image">
+      <GameCard v-for="(game, index) in gameStateLocal" :key="index" :imageUrl="game.background_image">
 
         <template v-slot:card-game-name>
           <p class="card-game-name">{{ game.name }}</p>
@@ -45,12 +49,14 @@
         </template>
 
         <template v-slot:card-game-plataform-images>
-          <img v-if="verifyPlataform(game.parent_platforms, 3)" src="../assets/img/icons/xbox-icon.svg" alt="Plataform icon">
-          <img v-if="verifyPlataform(game.parent_platforms, 2)" src="../assets/img/icons/ps-icon.svg" alt="Plataform icon">
-          <img v-if="verifyPlataform(game.parent_platforms, 1)" src="../assets/img/icons/win-icon.svg" alt="Plataform icon">
+          <img v-if="verifyPlataform(game.parent_platforms, 3)" src="../assets/img/icons/xbox-icon.svg"
+            alt="Plataform icon">
+          <img v-if="verifyPlataform(game.parent_platforms, 2)" src="../assets/img/icons/ps-icon.svg"
+            alt="Plataform icon">
+          <img v-if="verifyPlataform(game.parent_platforms, 1)" src="../assets/img/icons/win-icon.svg"
+            alt="Plataform icon">
         </template>
       </GameCard>
-
     </section>
 
   </main>
@@ -65,6 +71,7 @@ import GameCard from '../components/GameCard.vue'
 
 // Import store
 import { useStore } from 'vuex'
+import { ref } from 'vue'
 
 export default {
   name: 'HomeView',
@@ -78,6 +85,13 @@ export default {
     // Store initialization
     const store = useStore()
 
+    // Variables
+    let isOnSearch = ref(false)
+    let showAnimation = ref(false)
+    let isOnSearchGender = ref(true)
+    let gameTypeTittle = ref('Recomended')
+    let gameStateLocal = ref(store.state.recomendedGames)
+
     if (!store.state.topGames[0]) {
       store.dispatch('storeTopGames')
     }
@@ -85,20 +99,93 @@ export default {
       store.dispatch('storeRecomendedGames')
     }
 
-
     const verifyPlataform = (parentPlatforms, id) => {
-    for(let i = 0; i < parentPlatforms.length; i++) {
-        if(parentPlatforms[i].platform.id == id) {
-            return true
+      for (let i = 0; i < parentPlatforms.length; i++) {
+        if (parentPlatforms[i].platform.id == id) {
+          return true
         }
+      }
+      return false
     }
-    return false
-}
 
+    const removeAnimation = () => {
+      showAnimation.value = false
+    }
+
+    const apperAnimation = () => {
+      showAnimation.value = true
+    }
+
+    const onSearch = (eventData) => {
+      if (eventData && !store.state.games.search) {
+        isOnSearchGender.value = false
+        isOnSearch.value = true
+        apperAnimation()
+        return
+      }
+
+      if (!eventData) {
+        isOnSearch.value = false
+        isOnSearchGender.value = true
+        removeAnimation()
+        gameStateLocal.value = store.state.recomendedGames
+        gameTypeTittle.value = 'Recomended'
+      }
+    }
+
+    const onGenres = async (genres) => {
+      if (genres == 'all') {
+        isOnSearch.value = false
+        gameTypeTittle.value = 'Recomended'
+        gameStateLocal.value = store.state.recomendedGames
+        return
+      }
+
+      isOnSearch.value = true
+      isOnSearchGender.value = false
+      apperAnimation()
+
+      await store.dispatch('storeGames', {
+        type: 'genres',
+        genres
+      })
+
+      removeAnimation()
+      isOnSearchGender.value = true
+
+      gameStateLocal.value = store.state.games[genres]
+      gameTypeTittle.value = genres.charAt(0).toUpperCase() + genres.slice(1)
+    }
+
+    const searched = async (search) => {
+
+      isOnSearch.value = true
+      isOnSearchGender.value = false
+      apperAnimation()
+
+      await store.dispatch('storeGames', {
+        type: 'search',
+        search: search.search
+      })
+
+      removeAnimation()
+      isOnSearchGender.value = true
+
+      gameStateLocal.value = store.state.games.search
+      gameTypeTittle.value = 'Searched'
+    }
 
     return {
       store,
-      verifyPlataform
+      verifyPlataform,
+      onSearch,
+      isOnSearch,
+      onGenres,
+      gameTypeTittle,
+      gameStateLocal,
+      isOnSearchGender,
+      showAnimation,
+      searched
     }
   }
 }
@@ -133,6 +220,7 @@ main {
   color: #01080D;
   padding: 0rem 0.7rem;
   line-height: 2rem;
+  cursor: pointer;
 }
 
 .tittle-medium {
@@ -149,5 +237,10 @@ main {
 
 .cards {
   display: inline-flex;
+}
+
+.loading-animation {
+  margin: auto;
+  margin-top: 7rem;
 }
 </style>
