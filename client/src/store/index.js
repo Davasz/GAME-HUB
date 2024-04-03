92ae9c28c9512c66839160fdbe2b2c6268b6e38b
Davasz <davidavi200615@gmail.com>
2024-04-03 11:28:38 -0300

    ⚙️Feat: Add genres and search at store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,10 +1,21 @@
 import { createStore } from 'vuex'
 import axios from 'axios';
 
+const API_KEY = "9bd59e06c3c54bebb78dd73797d56178"
+
 export default createStore({
   state: {
     topGames: [],
-    recomendedGames: []
+    recomendedGames: [],
+    games: {
+      action: [],
+      indie: [],
+      arcade: [],
+      shooter: [],
+      casual: [],
+      puzzle: [],
+      search: []
+    }
   },
   getters: {
   },
@@ -18,11 +29,24 @@ export default createStore({
       data.forEach(element => {
         state.recomendedGames.push(element)
       });
+    },
+    storeGames(state, data) {
+      if (data.data.type === 'genres') {
+        let local = data.data.genres
+        data.response.forEach(element => {
+          if (!state.games[local].some(game => game.id === element.id)) state.games[local].push(element)
+        })
+      } else {
+        state.games.search = []
+        data.response.forEach(element => {
+          state.games.search.push(element)
+        })
+      }
     }
   },
   actions: {
     async storeTopGames({ commit }) {
-      const API_KEY = "9bd59e06c3c54bebb78dd73797d56178"
+
 
       try {
         const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=10`).then(e => e.data.results)
@@ -33,11 +57,32 @@ export default createStore({
     },
 
     async storeRecomendedGames({ commit }) {
-      const API_KEY = "9bd59e06c3c54bebb78dd73797d56178"
 
       try {
         const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=10&page=2`).then(e => e.data.results)
         commit('storeRecomendedGames', response)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async storeGames({ commit }, data) {
+
+      let query = ''
+
+      if (data.type == 'genres') {
+        query = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=10&genres=${data.genres}`
+      } else {
+        console.log(data.search)
+        query = `https://api.rawg.io/api/games?key=${API_KEY}&page_size=10&search=${data.search}`
+      }
+
+      try {
+        const response = await axios.get(query).then(e => e.data.results)
+        commit('storeGames', {
+          response,
+          data
+        })
       } catch (error) {
         console.log(error)
       }
