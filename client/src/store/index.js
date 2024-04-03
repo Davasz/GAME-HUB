efa7a7fca8415936497899652bc226d263cb4eb3
Davasz <davidavi200615@gmail.com>
2024-04-03 08:33:49 -0300

    ⚙️Feat: Add methods to Store

--- client/src/store/index.js
+++ client/src/store/index.js
@@ -1,14 +1,46 @@
 import { createStore } from 'vuex'
+import axios from 'axios';
 
 export default createStore({
   state: {
+    topGames: [],
+    recomendedGames: []
   },
   getters: {
   },
   mutations: {
+    storeTopGames(state, data) {
+      data.forEach(element => {
+        state.topGames.push(element)
+      });
+    },
+    storeRecomendedGames(state, data) {
+      data.forEach(element => {
+        state.recomendedGames.push(element)
+      });
+    }
   },
   actions: {
-  },
-  modules: {
+    async storeTopGames({ commit }) {
+      const API_KEY = "9bd59e06c3c54bebb78dd73797d56178"
+
+      try {
+        const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=10`).then(e => e.data.results)
+        commit('storeTopGames', response)
+      } catch (error) {
+        console.log(error)
+      }
+    },
+
+    async storeRecomendedGames({ commit }) {
+      const API_KEY = "9bd59e06c3c54bebb78dd73797d56178"
+
+      try {
+        const response = await axios.get(`https://api.rawg.io/api/games?key=${API_KEY}&page_size=10&page=2`).then(e => e.data.results)
+        commit('storeRecomendedGames', response)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 })
