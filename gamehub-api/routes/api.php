a0cf2d2d50202d01599cc4e51d95a9d486a2d008
Davasz <davidavi200615@gmail.com>
2024-04-08 13:11:57 -0300

    🪧Route: Auth routes

--- gamehub-api/routes/api.php
+++ gamehub-api/routes/api.php
@@ -1,18 +1,27 @@
 <?php
 
+use App\Http\Controllers\GameLikeController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
-Route::group(["prefix" => "users"], function () {
-    Route::get("/", [UserController::class, "index"])->name("index");
-    Route::get("/{id}", [UserController::class, "show"])->name("show")->middleware("auth:sanctum");
-    Route::post("/", [UserController::class, "store"])->name("store");
-    Route::put("/{id}", [UserController::class, "update"])->name("update");
-    Route::delete("/{id}", [UserController::class, "destroy"])->name("destroy");
+Route::group(["prefix" => "user"], function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::get("/", [UserController::class, "show"])->name("show");
+        Route::put("/{id}", [UserController::class, "update"])->name("update");
+        Route::delete("/", [UserController::class, "destroy"])->name("destroy");
+    });
 });
 
 
-Route::group(["prefix"=> "/auth"], function () {
-    Route::post("/", [LoginController::class,"login"])->name("login");
+Route::group(["prefix" => "/auth"], function () {
+    Route::post("/login", [LoginController::class, "login"])->name("login");
+    Route::post("/register", [UserController::class, "store"])->name("store");
+});
+
+Route::group(["prefix" => "/games"], function () {
+    Route::middleware(['auth:sanctum'])->group(function () {
+        Route::post("/", [GameLikeController::class, "store"])->name("store");
+        Route::delete("/{id}", [GameLikeController::class, "destroy"])->name("destroy");
+    });
 });
