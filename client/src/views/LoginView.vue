0186b8f60b9d22648ef7e6f68e01a86d36d04556
Davasz <davidavi200615@gmail.com>
2024-04-10 08:08:42 -0300

    ✨Feat: Login view

---
+++ client/src/views/LoginView.vue
@@ -0,0 +1,199 @@
+<template>
+    <main>
+        <div class="main">
+            <input type="checkbox" id="chk" aria-hidden="true">
+
+            <div class="login">
+                <form @submit.prevent="handleSubmit('login')" class="form">
+                    <label for="chk" aria-hidden="true">Log in</label>
+                    <input v-model="email" class="input" type="email" name="email" placeholder="Email" required>
+                    <input v-model="password" class="input" type="password" name="password" placeholder="Password"
+                        required>
+                    <button>Login</button>
+                </form>
+            </div>
+
+            <div class="register">
+                <form @submit.prevent="handleSubmit('register')" class="form register-form">
+                    <label class="register-label" for="chk" aria-hidden="true">Register</label>
+                    <input v-model="name" class="input" type="text" name="txt" placeholder="Name" required>
+                    <input v-model="email" class="input" type="email" name="email" placeholder="Email" required>
+                    <input v-model="password" class="input" type="password" name="pswd" placeholder="Password" min="8"
+                        required>
+                    <button>Register</button>
+                </form>
+            </div>
+        </div>
+    </main>
+</template>
+
+<script>
+// Import router
+import { useRouter } from 'vue-router'
+
+// Import store
+import { useStore } from 'vuex'
+
+// Import vue functions
+import { ref } from 'vue'
+
+export default {
+    setup() {
+        const router = useRouter()
+        const store = useStore()
+
+        let name = ref()
+        let email = ref()
+        let password = ref()
+
+        const handleSubmit = async (type) => {
+            if (type == 'login') {
+                await store.dispatch('login', {
+                    email: email.value,
+                    password: password.value
+                })
+                router.push('/user')
+            } else {
+                const response = await store.dispatch('register', {
+                    name: name.value,
+                    email: email.value,
+                    password: password.value
+                })
+                console.log(response)
+            }
+        }
+
+        return {
+            store,
+            name,
+            email,
+            password,
+            handleSubmit
+        }
+    }
+}
+</script>
+
+<style scoped>
+main {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    font-family: 'Trueno';
+    overflow: hidden;
+    position: absolute;
+    height: 100vh;
+    width: 100vw;
+}
+
+.main {
+    min-width: 24rem;
+    display: flex;
+    flex-direction: column;
+    overflow: hidden;
+    height: 100%;
+    border-radius: 12px;
+    box-shadow: 7px 7px 10px 3px #24004628;
+}
+
+.form {
+    display: flex;
+    flex-direction: column;
+    gap: 14px;
+    padding: 24px;
+}
+
+#chk {
+    display: none;
+}
+
+.login {
+    position: relative;
+    width: 100%;
+    height: 55%;
+    margin-top: 4rem;
+    margin-bottom: 7.5rem;
+}
+
+.login label {
+    margin: 25% 0 5%;
+}
+
+label {
+    color: #fff;
+    font-size: 2rem;
+    justify-content: center;
+    display: flex;
+    font-weight: bold;
+    cursor: pointer;
+    transition: .5s ease-in-out;
+}
+
+.input {
+    width: 100%;
+    height: 40px;
+    background: #e0dede;
+    padding: 10px;
+    border: none;
+    outline: none;
+    border-radius: 4px;
+}
+
+.register-label {
+    margin-bottom: 2rem;
+}
+
+.register-form {
+    height: 50rem;
+}
+
+form {
+    display: flex;
+    align-items: center;
+}
+
+.register {
+    background: #eee;
+    height: 90rem;
+    border-radius: 110% / 10%;
+    transform: translateY(5%);
+    transition: .8s ease-in-out;
+}
+
+.register label {
+    color: #0E161F;
+    transform: scale(.6);
+}
+
+#chk:checked~.register {
+    transform: translateY(-49%);
+}
+
+#chk:checked~.register label {
+    transform: scale(1);
+    margin: 10% 0 5%;
+}
+
+#chk:checked~.login label {
+    transform: scale(.6);
+    margin: 5% 0 5%;
+}
+
+.form button {
+    width: 85%;
+    height: 40px;
+    margin: 12px auto 10%;
+    color: #fff;
+    background: #0fa914;
+    font-size: 1rem;
+    font-weight: bold;
+    border: none;
+    border-radius: 4px;
+    cursor: pointer;
+    transition: .2s ease-in;
+}
+
+.form button:hover {
+    background-color: #15d61b;
+}
+</style>
