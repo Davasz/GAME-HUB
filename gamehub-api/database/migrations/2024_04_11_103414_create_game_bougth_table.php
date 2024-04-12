ed344440bdedda3cd4264824a5844c16aeacb9b4
Davasz <davidavi200615@gmail.com>
2024-04-12 11:14:43 -0300

    ⚙️Feat: Games Migrations

--- gamehub-api/database/migrations/2024_04_11_103414_create_game_bougth_table.php
+++ gamehub-api/database/migrations/2024_04_11_103414_create_game_bougth_table.php
@@ -4,14 +4,13 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
-        Schema::create('game_likes', function (Blueprint $table) {
+        Schema::create('game_bougths', function (Blueprint $table) {
             $table->id();
             $table->string('game_slug');
             $table->unsignedBigInteger('user_id');
@@ -26,6 +25,6 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::dropIfExists('game_like');
+        Schema::dropIfExists('game_bougth');
     }
 };
